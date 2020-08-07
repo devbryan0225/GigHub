421040bd4e65c884c4efce59230a6cc3b9997024
devbryan0225 <devbryan0225@users.noreply.github.com>
2020-08-08 01:19:14 +0800

    Create artists I'm following page.

---
+++ GigHub/Controllers/ArtistsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var followings = _context.Followings.Where(f => f.FollowerId == userId).Select(f => f.Followee).ToList();
+
+            return View(followings);
+        }
+    }
+}
