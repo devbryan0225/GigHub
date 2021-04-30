56d14bf99098a4e5e3bb5c8f611ad10f2f87f413
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-04-30 16:53:35 +0800

    Implement automapper.

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+using GigHub.Controllers.Api;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<ApplicationUser, UserDto>();
+            Mapper.CreateMap<Gig, GigDto>();
+            Mapper.CreateMap<Notification, NotificationDto>();
+        }
+    }
+}
--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
 
 namespace GigHub
 {
@@ -9,6 +10,7 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<App_Start.MappingProfile>());
             GlobalConfiguration.Configure(App_Start.WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
